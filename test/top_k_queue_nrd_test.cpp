5203def19dc90898bca8499a8d765011e1aa53db
chrip <chrisschaefer@uni-koblenz.de>
2014-10-10 18:03:30 +0200

    more tests

--- test/top_k_queue_nrd_test.cpp
+++ test/top_k_queue_nrd_test.cpp
@@ -6,18 +6,23 @@
 #include <fstream>
 #include "gtest/gtest.h"
 
-void tryOutParameters(bool sortVectorspace){
-	size_t topK = 3;
-	TopKQueueNRD tkqNRD = TopKQueueNRD();
-
+void loadVectorSpace(TopKQueueNRD& tkqNRD, const std::string& pathToVectorSpaceJson) {
 	//    std::ifstream input0( "/home/chrisschaefer/workspace/topk/job.json" );
-	std::ifstream input0("test_data/vector_space.json");
+	std::ifstream input0(pathToVectorSpaceJson);
 	int i = 0;
 	for (std::string line; getline(input0, line); i++){
 		vec vector = vec();
 		util::stringToVec(line, vector);
 		tkqNRD.addVector(util::getJsonInt(line, "id"), vector);
 	}
+}
+
+void tryOutParameters(bool sortVectorspace){
+	size_t topK = 3;
+	TopKQueueNRD tkqNRD = TopKQueueNRD();
+
+	//   loadVectorSpace(tkqNRD, "/home/chrisschaefer/workspace/topk/job.json"); 
+	loadVectorSpace(tkqNRD, "test_data/vector_space.json");
 
 	tkqNRD.buildIndex(1, 1, 0, sortVectorspace);
 	std::cout << tkqNRD.getVectorSpaceSize() << " vectors of length " << tkqNRD.getVectorSpace().getVectorSpacePyramide()[0][0].size() << std::endl;
@@ -49,14 +54,19 @@ void tryOutParameters(bool sortVectorspace){
 						<< " compressionBlockCols: " << compressionBlockCols
 						<< " compressionBlockRows: " << compressionBlockRows;
 					end = util::getTimestampInMilliseconds();
-					std::cout << "compressionLevels: " << compressionLevels
-						<< ", compressionBlockCols: " << compressionBlockCols
-						<< ", compressionBlockRows: " << compressionBlockRows
-						<< ", time: " << (end - start) << " ms" << std::endl;
+					//std::cout << "compressionLevels: " << compressionLevels
+					//	<< ", compressionBlockCols: " << compressionBlockCols
+					//	<< ", compressionBlockRows: " << compressionBlockRows
+					//	<< ", time: " << (end - start) << " ms" << std::endl;
 				}
 			}
 		}
 	}
+}
+
+TEST(tkqNrdTest, tryOutParametersUnsortedVectorspace) {
+	bool sortVectorspace = false;
+	tryOutParameters(sortVectorspace);
 }
 
 TEST(tkqNrdTest, tryOutParametersSortedVectorspace) {
@@ -64,9 +74,86 @@ TEST(tkqNrdTest, tryOutParametersSortedVectorspace) {
 	tryOutParameters(sortVectorspace);
 }
 
-TEST(tkqNrdTest, tryOutParametersUnsortedVectorspace) {
+typedef std::pair<std::string, int> stringInt;
+
+class StringIntComparison
+{
+public:
+	bool operator() (const stringInt& lhs, const stringInt& rhs) const
+	{
+		return (lhs.second < rhs.second);
+	}
+};
+
+typedef std::priority_queue<stringInt, std::vector<stringInt>, StringIntComparison> stringIntegerQueue;
+
+void findBestParameter(bool sortVectorspace){
+	size_t topK = 20;
+	TopKQueueNRD tkqNRD = TopKQueueNRD();
+
+	 //  loadVectorSpace(tkqNRD, "../../job.json"); 
+	loadVectorSpace(tkqNRD, "test_data/vector_space.json");
+
+	tkqNRD.buildIndex(1, 1, 0, sortVectorspace);
+	std::cout << tkqNRD.getVectorSpaceSize() << " vectors of length " << tkqNRD.getVectorSpace().getVectorSpacePyramide()[0][0].size() << std::endl;
+
+	stringIntegerQueue topKQueue = stringIntegerQueue();
+	for (size_t i = 0; i < topK; i++){
+		topKQueue.push({ "", 10000000 });
+	}
+
+	size_t compressionLevels = 1;
+	for (; compressionLevels < 2; compressionLevels++){
+		size_t compressionBlockRows = 2;
+		for (; compressionBlockRows < 4; compressionBlockRows++){
+			size_t compressionBlockCols = 2;
+			for (; compressionBlockCols < 4; compressionBlockCols++){
+
+				int64_t avgTime = 0;
+				int64_t counter = 0;
+				tkqNRD.buildIndex(compressionBlockRows, compressionBlockCols, compressionLevels);
+
+				std::ifstream input1("test_data/more_query_vectors.json");
+				for (std::string line; getline(input1, line); counter++)
+				{
+					if (counter == 2) break;
+					vec vector = vec();
+					util::stringToVec(line, vector);
+
+					int64_t start, end;
+					start = util::getTimestampInMilliseconds();
+					QueryVector qv = QueryVector(vector, compressionBlockRows, compressionLevels);
+					tkqNRD.executeTopK(qv, topK);
+					end = util::getTimestampInMilliseconds();
+
+					avgTime += end - start;
+				}
+				avgTime /= counter;
+				std::cout << avgTime << std::endl;
+				if (avgTime < topKQueue.top().second){
+					std::string s = "compressionLevels: " + std::to_string(compressionLevels)
+						+ ", compressionBlockCols: " + std::to_string(compressionBlockCols)
+						+ ", compressionBlockRows: " + std::to_string(compressionBlockRows);
+					topKQueue.pop();
+					topKQueue.push({ s, avgTime});
+				}
+			}
+		}
+	}
+	while (!topKQueue.empty()) {
+		std::cout << topKQueue.top().first << "," << topKQueue.top().second << std::endl;
+		topKQueue.pop();
+	}
+}
+
+TEST(tkqNrdTest, findBestParametersUnsortedVectorspace) {
 	bool sortVectorspace = false;
-	tryOutParameters(sortVectorspace);
+	findBestParameter(sortVectorspace);
+}
+
+TEST(tkqNrdTest, findBestParametersSortedVectorspace) {
+	bool sortVectorspace = true;
+	findBestParameter(sortVectorspace);
 }
 
 int main(int argc, char **argv) {
